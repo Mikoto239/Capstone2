1f70680509c509e3ca04d49e71a42870663b65db
ALMIKO JASPER L. CABALLES <89283730+Mikoto239@users.noreply.github.com>
2024-06-14 17:43:42 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,15 +7,9 @@ const HardwareRoute = require('./routes/hardware.js');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const http = require('http'); // Import the http module
-const socketIO = require('socket.io');
 require('dotenv').config();
 
-// MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGODB_URI);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
@@ -30,27 +24,6 @@ app.use('/api', HardwareRoute);
 
 app.use('/', homeRouter);
 
-// Create HTTP server
-const server = http.createServer(app);
-const io = socketIO(server); // Attach socket.io to the HTTP server
-
-// Define socket.io event handlers
-io.on('connection', (socket) => {
-  console.log('A client connected');
-
-  // Example: Listen for a custom event
-  socket.on('custom_event', (data) => {
-    console.log('Custom event received:', data);
-    // Example: Emit a response
-    io.emit('response_event', 'Response from server');
-  });
-
-  // Handle disconnection
-  socket.on('disconnect', () => {
-    console.log('A client disconnected');
-  });
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Node.js server listening on port ${PORT}`);
 });
