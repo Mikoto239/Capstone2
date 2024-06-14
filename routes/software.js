55239d6f2be1d13ed82ff79881554a7209c187b4
ALMIKO JASPER L. CABALLES <89283730+Mikoto239@users.noreply.github.com>
2024-06-14 17:40:53 +0800

    Update software.js

--- routes/software.js
+++ routes/software.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const {getlocation,turnoffhardware,turnOnhardware,userexist,deletetheft,alltheft, userregistration, deleteuser, hardwarestatus, changestatus, notification, allnotification,pinhistory} = require('../controllers/software');
+const {getlocation,turnoffhardware,turnOnhardware,userexist,deletetheft,theftalert, userregistration, deleteuser, hardwarestatus, changestatus, latestnotification,allnotification,pinhistory} = require('../controllers/software');
+const {authenticateToken} = require('../middleware/authorization');
 
-router.get('/getlocation',getlocation);
-router.post('/deletecurrentlocation',turnoffhardware);
-router.post('/currentlocation',turnOnhardware);
-router.post('/checkuserregister',userexist);
-router.post('/removetheftdetails',deletetheft);
-router.post('/gettheftdetails',alltheft);
 router.post('/userregister',userregistration);
-router.post('/deleteuser',deleteuser);
-router.get('/hardwarestatus',hardwarestatus);
-router.post('/changestatus',changestatus);
-router.post('/allhistory',allnotification);
-router.post('/getnotification',notification);
-router.post('/getpinhistory',pinhistory);
-
+router.post('/getlocation',authenticateToken,getlocation);
+router.post('/turnoff',authenticateToken,turnoffhardware);
+router.post('/currentlocation',authenticateToken,turnOnhardware);
+router.post('/checkuser',authenticateToken,userexist);
+router.post('/removetheftdetails',authenticateToken,deletetheft);
+router.post('/theftalert',authenticateToken,theftalert);
+router.post('/deleteuser',authenticateToken,deleteuser);
+router.post('/hardwarestatus',authenticateToken,hardwarestatus);
+router.post('/changestatus',authenticateToken,changestatus);
+router.post('/allnotification',allnotification);
+router.post('/getlatestnotification',authenticateToken,latestnotification);
+router.post('/getpinhistory',authenticateToken,pinhistory);
 
 module.exports = router;
 
