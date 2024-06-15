b97671aa8750572078a23d3aef07350c2e066f7f
ALMIKO JASPER L. CABALLES <89283730+Mikoto239@users.noreply.github.com>
2024-06-15 20:03:00 +0800

    Update hardware.js

--- routes/hardware.js
+++ routes/hardware.js
@@ -1,15 +1,17 @@
+
 const express = require('express');
 const router = express.Router();
-const { getcurrentpinlocation, send_alert, pinlocation, theftdetails, getusernumber, hardwareregistration, pinstatus } = require('../controllers/hardware.js');
+const { getcurrentpinlocation, send_alert, pinlocation, theftdetails, getusernumber, hardwareregistration, pinstatus ,send_theftalert} = require('../controllers/hardware.js');
 const { isHardwareOn } = require('../middleware/hardwarestatus');
 const {authenticateToken} = require('../middleware/authorization');
 
-router.post('/checkpinlocation', isHardwareOn,authenticateToken,getcurrentpinlocation);
-router.post('/send_alert',isHardwareOn,authenticateToken,send_alert);
-router.post('/pinthislocation',isHardwareOn,authenticateToken,pinlocation);
-router.post('/pinstatus', pinstatus,authenticateToken,);
-router.post('/sendtheftdetails',isHardwareOn,authenticateToken,theftdetails);
+router.post('/checkpinlocation',authenticateToken, isHardwareOn,getcurrentpinlocation);
+router.post('/send_alert',authenticateToken,isHardwareOn,send_alert);
+router.post('/pinthislocation',authenticateToken,isHardwareOn,pinlocation);
+router.post('/pinstatus',authenticateToken,pinstatus);
+router.post('/sendtheftdetails',authenticateToken,isHardwareOn,theftdetails);
 router.post('/hardwareregister',hardwareregistration);
-router.post('/usernumber',isHardwareOn, getusernumber,authenticateToken);
+router.post('/usernumber',authenticateToken,isHardwareOn, getusernumber);
+router.post('/currentvehiclelocation',authenticateToken,isHardwareOn,send_theftalert);
 
 module.exports = router;
