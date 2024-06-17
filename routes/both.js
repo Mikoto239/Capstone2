6ec2914af749556c6bb60f013b7ed387eef475f7
ALMIKO JASPER L. CABALLES <89283730+Mikoto239@users.noreply.github.com>
2024-06-17 16:41:31 +0800

    Update both.js

--- routes/both.js
+++ routes/both.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { hardwarestatus} = require('../controllers/both.js');
+const {mobilehardwarestatus, hardwarestatus} = require('../controllers/both.js');
 const { isHardwareOn } = require('../middleware/hardwarestatus');
 const {authenticateToken} = require('../middleware/authorization');
 
 router.post('/hardwarestatus',authenticateToken,isHardwareOn,hardwarestatus);
+router.post('/mobilehardwarestatus',authenticateToken,isHardwareOn,mobilehardwarestatus);
 
 module.exports = router;
