6e87184b4c7caf04ff11881355c30473debc7ba4
ALMIKO JASPER L. CABALLES <89283730+Mikoto239@users.noreply.github.com>
2024-06-17 16:41:46 +0800

    Update both.js

--- controllers/both.js
+++ controllers/both.js
@@ -4,10 +4,8 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const SECRET_KEY = process.env.SECRET_KEY;
 
-
-
-//check hardware status
-exports.hardwarestatus = async (req, res, next) => {
+// Check hardware status
+exports.mobilehardwarestatus = async (req, res, next) => {
   const { token } = req.body;
 
   try {
@@ -17,11 +15,12 @@ exports.hardwarestatus = async (req, res, next) => {
       return res.status(401).json({ message: 'Unauthorized Access!' });
     }
 
-    const decodedId = decoded.id; 
-    const userexist= await User.findById(decodedId);
-    if(!userexist){
-      res.status(404).json({message:"Not found!"});
+    const decodedId = decoded.id;
+    const userexist = await User.findById(decodedId);
+    if (!userexist) {
+      return res.status(404).json({ message: 'User not found!' });
     }
+
     const uniqueId = userexist.uniqueId;
     const hardware = await Hardware.findOne({ uniqueId });
 
@@ -29,7 +28,46 @@ exports.hardwarestatus = async (req, res, next) => {
       return res.status(404).json({ message: 'Hardware not found' });
     }
 
-    const hardwareStatus = hardware.status; 
+    const hardwareStatus = hardware.status;
+
+    return res.status(200).json({ status: hardwareStatus });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+
+exports.hardwarestatus = async (req, res, next) => {
+  const { token } = req.body;
+
+  try {
+
+    const decoded = jwt.verify(token, SECRET_KEY);
+
+    if (!decoded || !decoded.id) {
+      return res.status(401).json({ message: 'Unauthorized Access!' });
+    }
+
+    const decodedId = decoded.id;
+
+ 
+    const hardwareexist = await Hardware.findById(decodedId);
+    if (!hardwareexist) {
+      return res.status(404).json({ message: 'Hardware not found!' });
+    }
+
+    
+    const uniqueId = hardwareexist.uniqueId;
+
+  
+    const hardware = await Hardware.findOne({ uniqueId });
+    if (!hardware) {
+      return res.status(404).json({ message: 'Hardware not found!' });
+    }
+
+ 
+    const hardwareStatus = hardware.status;
 
     return res.status(200).json({ status: hardwareStatus });
   } catch (error) {
