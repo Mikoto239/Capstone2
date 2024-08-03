1a09201777e3c40d911b5c06b37cfd9b4c03dfb5
ALMIKO JASPER L. CABALLES <89283730+Mikoto239@users.noreply.github.com>
2024-08-04 01:56:51 +0800

    Update software.js

--- controllers/software.js
+++ controllers/software.js
@@ -185,6 +185,42 @@ exports.turnOnhardware = async (req, res, next) => {
 };
 
 
+exports.offrequest = async (req, res, next) => {
+  const {token } = req.body;
+ 
+  try {
+
+    const decoded = jwt.verify(token, SECRET_KEY);
+
+    if (!decoded || !decoded.id) {
+      return res.status(401).json({ message: 'Unauthorized Access!' });
+    }
+
+    const decodedId = decoded.id; 
+
+    const userexist= await User.findById(decodedId);
+    if(!userexist){
+      res.status(404).json({message:"Not found!"});
+    }
+    const uniqueId = userexist.uniqueId;
+    const hardware = await Hardware.findOneAndUpdate(
+      { uniqueId },
+      { pinlocation: false },
+      { new: true }
+    );
+    
+    if (!hardware) {
+      return res.status(400).json({ message: "Hardware not Found!" });
+    }
+
+    return res.status(200).json({message:"Successfully Turn Off!"});
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+
 
 
 //check the user if already register
