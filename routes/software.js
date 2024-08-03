5f9578681dbc3cebf6ce3da191f4dfba505a1007
ALMIKO JASPER L. CABALLES <89283730+Mikoto239@users.noreply.github.com>
2024-08-04 01:57:40 +0800

    Update software.js

--- routes/software.js
+++ routes/software.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {getlocation,turnoffhardware,turnOnhardware,userexist,deletetheft,theftalert, userregistration, deleteuser, changestatus, latestnotification,allnotification,pinhistory,send_theftalert,updateusernumber,sendtoken,mapnotification} = require('../controllers/software');
+const {offrequest,getlocation,turnoffhardware,turnOnhardware,userexist,deletetheft,theftalert, userregistration, deleteuser, changestatus, latestnotification,allnotification,pinhistory,send_theftalert,updateusernumber,sendtoken,mapnotification} = require('../controllers/software');
 const {authenticateToken} = require('../middleware/authorization');
 const { isHardwareOn } = require('../middleware/mobilehardwarestatus');
 router.post('/userregister',userregistration);
@@ -19,6 +19,7 @@ router.post('/currenttheftlocation',authenticateToken,isHardwareOn ,send_theftal
 router.post('/updateusernumber',authenticateToken,updateusernumber);
 router.post('/sendtoken',sendtoken);
 router.post('/latestmapnotification',authenticateToken,isHardwareOn,mapnotification);
+router.post('/offrequest',authenticateToken,offrequest);
 module.exports = router;
 
 
